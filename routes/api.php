98492209f59bd07cf53deefbd2938a800d465e25
glitchy <zschopper@gmail.com>
2024-02-15 10:43:23 +0100

    breeze api authentikáció

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,13 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
+require __DIR__.'/auth.php';
+
+Route::post('/login', [App\Http\Controllers\LoginController::class, 'login']);
+Route::post('/logout', [App\Http\Controllers\LoginController::class, 'logout'])->middleware('auth:api');
+// Route::group(['middleware'=>['auth:api']],function(){ Route::post('/logout', 'Api\AuthController@logout'); });
+
+
 // Route::get('drink/scheme', [\App\Http\Controllers\DrinkController::class, 'scheme']);
 Route::apiResource('drinks', \App\Http\Controllers\DrinkController::class);
 Route::get('categories/{category}/drinks', [\App\Http\Controllers\DrinkCategoryController::class, 'drinks']);
@@ -30,3 +38,34 @@ Route::fallback(function () {
         'message'   => __('Page not found.'),
     ], 404);
 });
+
+/*
+
+Route::post('/register', [App\Http\Controllers\Auth\RegisteredUserController::class, 'store'])
+                ->middleware('guest')
+                ->name('register');
+
+// Route::post('/login', [App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'store'])
+//                 ->middleware('guest')
+//                 ->name('login');
+
+Route::post('/forgot-password', [App\Http\Controllers\Auth\PasswordResetLinkController::class, 'store'])
+                ->middleware('guest')
+                ->name('password.email');
+
+Route::post('/reset-password', [App\Http\Controllers\Auth\NewPasswordController::class, 'store'])
+                ->middleware('guest')
+                ->name('password.store');
+
+Route::get('/verify-email/{id}/{hash}', App\Http\Controllers\Auth\VerifyEmailController::class)
+                ->middleware(['auth', 'signed', 'throttle:6,1'])
+                ->name('verification.verify');
+
+Route::post('/email/verification-notification', [App\Http\Controllers\Auth\EmailVerificationNotificationController::class, 'store'])
+                ->middleware(['auth', 'throttle:6,1'])
+                ->name('verification.send');
+
+// Route::post('/logout', [App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'destroy'])
+//                 ->middleware('auth')
+//                 ->name('logout');
+*/
