951394f7b82acf40641e5f6f0c4789e436eb6604
glitchy <zschopper@gmail.com>
2024-03-13 21:53:10 +0100

    E-mail küldés vendég regisztráció után

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Guest extends Authenticatable implements JWTSubject
+class Guest extends Authenticatable implements JWTSubject, MustVerifyEmail
 {
     use HasFactory, Notifiable, SoftDeletes;
 
