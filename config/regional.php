f155f0ec128d7d983e2710702ae313a7f75edc8a
glitchy <zschopper@gmail.com>
2024-03-13 21:53:39 +0100

    konfiguráció a nevek formázásához

---
+++ config/regional.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'en' => [
+        'name_format' => [
+            'LAST_NAME', // ha csupa nagybetűs az elem, a nevet is nagybetűsíti
+            'first_name',
+            'middle_name',
+        ]
+    ],
+    'hu' => [
+        'name_format' => [
+            'last_name',
+            'first_name',
+            'middle_name',
+        ]
+    ],
+];
